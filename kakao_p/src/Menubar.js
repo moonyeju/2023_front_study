4736b66627d03764f9f64467854efd7eb0776940
moonyeju <moonyeju8510@gmail.com>
2023-01-26 00:27:08 +0900

    modal update

--- kakao_p/src/Menubar.js
+++ kakao_p/src/Menubar.js
@@ -1,10 +1,14 @@
 import "./Header.css";
 import React, { useState } from "react";
 import Menu1 from "./Menu1.js";
+import Menu2 from "./Menu2.js";
+import Menu3 from "./Menu3.js";
 
 function Menubar() {
   const [isHovering, setIsHovering] = useState(false);
-  const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen1, setModalOpen1] = useState(false);
+  const [modalOpen2, setModalOpen2] = useState(false);
+  const [modalOpen3, setModalOpen3] = useState(false);
 
   const handleMouseOver = () => {
     setIsHovering(true);
@@ -21,11 +25,11 @@ function Menubar() {
         onMouseOver={handleMouseOver}
         onMouseOut={handleMouseOut}
         onClick={() => {
-          setModalOpen(!modalOpen);
+          setModalOpen1(!modalOpen1);
         }}
       >
         카카오
-        {modalOpen ? <Menu1 /> : null}
+        {modalOpen1 ? <Menu1 /> : null}
       </li>
 
       <li
@@ -39,15 +43,23 @@ function Menubar() {
         className={isHovering ? "hov" : "item"}
         onMouseOver={handleMouseOver}
         onMouseOut={handleMouseOut}
+        onClick={() => {
+          setModalOpen2(!modalOpen2);
+        }}
       >
         기술과 서비스
+        {modalOpen2 ? <Menu2 /> : null}
       </li>
       <li
         className={isHovering ? "hov" : "item"}
         onMouseOver={handleMouseOver}
         onMouseOut={handleMouseOut}
+        onClick={() => {
+          setModalOpen3(!modalOpen3);
+        }}
       >
         약속과 책임
+        {modalOpen3 ? <Menu3 /> : null}
       </li>
     </ul>
   );
