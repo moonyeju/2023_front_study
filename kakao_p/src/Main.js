ec3a4da6c627f128247b02d2d53cf7ce28a5414b
moonyeju <moonyeju8510@gmail.com>
2023-01-29 00:17:56 +0900

    container update

--- kakao_p/src/Main.js
+++ kakao_p/src/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Main.css";
 import calculate from "./images/cal.png";
+import { AiFillFund } from "react-icons/ai";
 
 function Main() {
   const now = new Date();
@@ -10,17 +11,34 @@ function Main() {
   let todayDate = now.getDate();
   return (
     <div className="main">
-      <div>
-        <div className="word1">
-          <img className="image" src={calculate} />
-          <h1>오늘의 카카오</h1>
-        </div>
-        <h1 className="word2">
-          {todayMonth}월 {todayDate}일 {dayOfWeek}요일 소식
+      <div className="word1">
+        <img className="image" src={calculate} />
+        <h1>오늘의 카카오</h1>
+      </div>
+      <div className="word2">
+        <h1>
+          {todayMonth}월 {todayDate}일 {dayOfWeek}요일 소식입니다
         </h1>
       </div>
-      <div className="container">
-        <div className="balloon">주가정보</div>
+      <div className="r">
+        <div className="container1">
+          <div className="r">
+            <div>
+              <AiFillFund size={40} />
+            </div>
+
+            <div className="balloon">히스토리</div>
+          </div>
+          <h1>인공지능이 그린 그림은 창작물일까?</h1>
+        </div>
+        <div className="c">
+          <div className="container2">
+            <div className="balloon">주가정보</div>
+          </div>
+          <div className="container2">
+            <div className="balloon">주가정보</div>
+          </div>
+        </div>
       </div>
     </div>
   );
